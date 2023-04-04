7697bb8515da0e153c3e4bcb6be90b667e78fdb4
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-04-04 11:30:26 +0000

    save

--- tpdne/src/App.jsx
+++ tpdne/src/App.jsx
@@ -9,10 +9,32 @@ const App = () => {
     async function fetchData() {
       try {
         const response = await fetch(
-          `https://randomuser.me/api/?results=5&gender=${gender}`
+          `https://randomuser.me/api/?results=5&gender=${gender}&inc=name,email,picture,login,dob,location,nat`
         );
         const data = await response.json();
-        setUserData(data.results);
+        setUserData(
+          data.results.map((user) => {
+            const { first, last } = user.name;
+            const email = user.email;
+            const picture = user.picture.large;
+            const age = user.dob.age;
+            const dob = user.dob.date;
+            const nationality = user.nat;
+            const password = `${first.toLowerCase()}${last.toLowerCase()}${age}${nationality.toLowerCase()}`;
+            const passwordStrength = password.length > 10 ? "strong" : "weak";
+            return {
+              first,
+              last,
+              email,
+              picture,
+              age,
+              dob,
+              nationality,
+              password,
+              passwordStrength,
+            };
+          })
+        );
       } catch (error) {
         console.error(error);
       }
@@ -42,11 +64,17 @@ const App = () => {
       <div>
         {userData.map((user, index) => (
           <div key={index} className="user">
-            <img src={user.picture.large} alt="User avatar" />
+            <img src={user.picture} alt="User avatar" />
             <p>
-              Name: {user.name.first} {user.name.last}
+              Name: {user.first} {user.last}
             </p>
             <p>Email: {user.email}</p>
+            <p>Age: {user.age}</p>
+            <p>Nationality: {user.nationality}</p>
+            <p>Date of Birth: {new Date(user.dob).toLocaleDateString()}</p>
+            <p>
+              Password: {user.password} ({user.passwordStrength})
+            </p>
           </div>
         ))}
       </div>
